ce3448e339ceb610ef06d85d46d88ceabb23d752
mskanogo <m.kanogo@alustudent.com>
2026-03-03 21:23:40 +0200

    all tasks

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that queries Reddit API and prints top 10 hot posts"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json()
+    posts = data.get("data", {}).get("children", [])
+    for post in posts:
+        print(post.get("data", {}).get("title"))
