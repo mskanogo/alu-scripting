ce7bcf9103ea0a8fb629db4f0d8e7ff79498d73f
mskanogo <m.kanogo@alustudent.com>
2026-03-03 21:43:38 +0200

    Fix 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "MyBot/1.0"}
+    headers = {"User-Agent": "linux:myredditapp:v1.0 (by /u/yourusername)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
