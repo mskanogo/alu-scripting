6779cf363d9c60557b495f74e05e5d66172828a5
mskanogo <m.kanogo@alustudent.com>
2026-03-14 14:49:19 +0200

    Fix PEP8 and add README for api_advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,16 +6,10 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "python:api_advanced:v1.0 (by /u/api_advanced)"
-    }
+    headers = {"User-Agent": "python:api_advanced:v1.0 (by /u/api_advanced)"}
     params = {"limit": 10}
-    response = requests.get(
-        url,
-        headers=headers,
-        params=params,
-        allow_redirects=False
-    )
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
