dbda5fcf78d5effd1f87b0d7818f83b9e63b7d4e
mskanogo <m.kanogo@alustudent.com>
2026-03-14 14:55:38 +0200

    Fix top_ten User-Agent to match 0-subs

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,17 +6,14 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (compatible; MyBot/1.0)"}
+    headers = {"User-Agent": "MyBot/1.0 by /u/myuser"}
     params = {"limit": 10}
-    try:
-        response = requests.get(url, headers=headers,
-                                params=params, allow_redirects=False)
-        if response.status_code != 200:
-            print(None)
-            return
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-        for post in posts:
-            print(post.get("data", {}).get("title"))
-    except Exception:
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+    data = response.json()
+    posts = data.get("data", {}).get("children", [])
+    for post in posts:
+        print(post.get("data", {}).get("title"))
